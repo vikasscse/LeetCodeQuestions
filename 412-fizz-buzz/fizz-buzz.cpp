f68998d1257fe425707badefea72267517366974
vikasscse <vishalkumarstm497@gmail.com>
2025-12-01 18:27:40 +0530

    Time: 2 ms (43.13%) | Memory: 11.8 MB (14.87%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>st;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 && i%5==0) st.push_back("FizzBuzz");
+            else if(i%5==0) st.push_back("Buzz");
+            else if(i%3==0) st.push_back("Fizz");
+            else st.push_back(to_string(i));
+        }
+        return st;
+    }
+};
