b2d0adffa43948835388dec24dfcfda7f2ad5211
vikasscse <vishalkumarstm497@gmail.com>
2026-03-14 21:46:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (31.26%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n = s.size();
+        int low = 0, high = n;
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'I') {
+                ans.push_back(low);
+                low++;
+            } else {
+                ans.push_back(high);
+                high--;
+            }
+        }
+        ans.push_back(low);
+        return ans;
+    }
+};
