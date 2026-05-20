53f0f1a94b7a502434ad3b16175e2bc04afc07af
vikasscse <vishalkumarstm497@gmail.com>
2026-05-20 21:58:10 +0530

    Time: 27 ms (95.82%) | Memory: 13.1 MB (86.84%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
 
-    int solve(int i,int j,string &s,string &temp,int dp[][1001])
+    int solve(int i,int j,string &s,int dp[][1001])
     {
-        if(i==s.size() || j==s.size())
+        if(i>j)
         return 0;
 
         if(dp[i][j] != -1)
-        return dp[i][j];
+        return dp[i][j]; 
 
-        if(s[i] == temp[j])
-        return dp[i][j] = 1+solve(i+1,j+1,s,temp,dp);
+        if(i==j)
+        return dp[i][j]= 1;
 
-        return dp[i][j] = max(solve(i+1,j,s,temp,dp),solve(i,j+1,s,temp,dp));
+        if(s[i]==s[j])
+        return dp[i][j]=2+solve(i+1,j-1,s,dp);
+
+        return dp[i][j] = max(solve(i+1,j,s,dp),solve(i,j-1,s,dp));
     }
     int longestPalindromeSubseq(string s) {
         int dp[1001][1001];
         for(int i=0;i<1001;i++)
-        for(int j=0;j<1001;j++)
-        dp[i][j]=-1;
-        string temp=s;
-        reverse(temp.begin(),temp.end());
-        return solve(0,0,s,temp,dp);
+        {
+            for(int j=0;j<1001;j++)
+            {
+                dp[i][j] =-1;
+            }
+        }
+        int i=0;
+        int j=s.size()-1;
+        return solve(i,j,s,dp);
     }
 };
